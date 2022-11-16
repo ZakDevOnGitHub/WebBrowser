523d799c743cba6793e5eef08ac00d6672a266e2
Zak Mohamed <Zakm741@hotmail.com>
2022-11-16 09:56:44 -0500

    UPDATE: Now powered by chromium, browsing just got a lot easier, faster, and safer!

--- Web Browser/Form1.cs
+++ Web Browser/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using CefSharp.WinForms;
+using CefSharp;
 
 namespace Web_Browser
 {
@@ -18,35 +20,46 @@ namespace Web_Browser
 			InitializeComponent();
 		}
 
+		ChromiumWebBrowser chrome;
 		private void pictureBox1_Click(object sender, EventArgs e)
 		{
-			webBrowser3.GoBack();
+			if (chrome.CanGoBack)
+			{
+				chrome.Back();
+			}
 		}
 
 		private void pictureBox4_Click(object sender, EventArgs e)
 		{
-			webBrowser3.GoForward();
+			if (chrome.CanGoForward)
+			{
+				chrome.Forward();
+			}
 		}
 
 		private void pictureBox2_Click(object sender, EventArgs e)
 		{
-			webBrowser3.Refresh();
+			chrome.Refresh();
 		}
 
 		private void pictureBox3_Click(object sender, EventArgs e)
 		{
-			webBrowser3.Navigate(SearchURLBOX.Text);
+			chrome.Load(SearchURLBOX.Text);
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			webBrowser3.Navigate(HomeSet.Text);
-
+			CefSettings settings = new CefSettings();
+			Cef.Initialize(settings);
+			SearchURLBOX.Text = "https://www.bing.com";
+			chrome = new ChromiumWebBrowser(SearchURLBOX.Text);
+			this.BTab.Controls.Add(chrome);
+			chrome.Dock = DockStyle.Fill;
+			chrome.AddressChanged += Chrome_AddressChanged;
 			SettingsMenu.Hide();
 			Settings.Hide();
 			PersonalizationLabel.Hide();
 			PersonalizationImage.Hide();
-			PersonalizationPage.Hide();
 			HomePageLabel.Hide();
 			HomeSet.Hide();
 			ColorLabel.Hide();
@@ -99,11 +112,9 @@ namespace Web_Browser
 			pictureBox58.Hide();
 			pictureBox59.Hide();
 			BookmarkList.Hide();
-			BookmarkPage.Hide();
 			BookmarkSet.Hide();
 			BookmarkLabel.Hide();
 			BookmarkImage.Hide();
-			AddBookmarkButton.Hide();
 			SubtractBookmarkButton.Hide();
 			CloseButton.Hide();
 			VersionLabel.Hide();
@@ -116,7 +127,7 @@ namespace Web_Browser
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-		
+
 		}
 
 		private void Bookmark_SelectedIndexChanged(object sender, EventArgs e)
@@ -126,17 +137,17 @@ namespace Web_Browser
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-           
+
 		}
 
 		private void pictureBox6_Click(object sender, EventArgs e)
 		{
-			BookmarkList.Items.Clear();
+			BookmarkList.Items.Add(SearchURLBOX.Text);
 		}
 
 		private void pictureBox5_Click(object sender, EventArgs e)
 		{
-			webBrowser3.Navigate(HomeSet.Text);
+			chrome.Load(HomeSet.Text);
 		}
 
 		private void pictureBox23_Click(object sender, EventArgs e)
@@ -387,7 +398,6 @@ namespace Web_Browser
 			Settings.Hide();
 			PersonalizationLabel.Hide();
 			PersonalizationImage.Hide();
-			PersonalizationPage.Hide();
 			HomePageLabel.Hide();
 			HomeSet.Hide();
 			ColorLabel.Hide();
@@ -428,7 +438,7 @@ namespace Web_Browser
 			pictureBox46.Hide();
 			pictureBox47.Hide();
 			pictureBox48.Hide();
-			pictureBox49.Hide(); 
+			pictureBox49.Hide();
 			pictureBox50.Hide();
 			pictureBox52.Hide();
 			pictureBox53.Hide();
@@ -439,13 +449,10 @@ namespace Web_Browser
 			pictureBox58.Hide();
 			pictureBox59.Hide();
 			BookmarkList.Hide();
-			BookmarkPage.Hide();
 			BookmarkSet.Hide();
 			BookmarkLabel.Hide();
 			BookmarkImage.Hide();
-			AddBookmarkButton.Hide();
 			SubtractBookmarkButton.Hide();
-			AddBookmarkButton.Hide();
 			SubtractBookmarkButton.Hide();
 			CloseButton.Hide();
 			VersionLabel.Hide();
@@ -458,14 +465,13 @@ namespace Web_Browser
 
 		private void SubtractBookmarkButton_Click(object sender, EventArgs e)
 		{
-              BookmarkList.Items.Remove(BookmarkList.CheckedItems);
+			BookmarkList.Items.Remove(BookmarkList.CheckedItems);
 		}
 
 		private void PersonalizationLabel_Click(object sender, EventArgs e)
 		{
 			PersonalizationLabel.Show();
 			PersonalizationImage.Show();
-			PersonalizationPage.Show();
 			HomePageLabel.Show();
 			HomeSet.Show();
 			ColorLabel.Show();
@@ -517,19 +523,15 @@ namespace Web_Browser
 			pictureBox58.Show();
 			pictureBox59.Show();
 			BookmarkList.Hide();
-			BookmarkPage.Hide();
 		}
 
 		private void BookmarkLabel_Click(object sender, EventArgs e)
 		{
 			BookmarkList.Show();
-			BookmarkPage.Show();
 			BookmarkSet.Show();
 			BookmarkLabel.Show();
 			BookmarkImage.Show();
-			AddBookmarkButton.Show();
 			SubtractBookmarkButton.Show();
-			PersonalizationPage.Hide();
 			HomePageLabel.Hide();
 			HomeSet.Hide();
 			ColorLabel.Hide();
@@ -593,6 +595,268 @@ namespace Web_Browser
 			PersonalizationImage.Show();
 			CloseButton.Show();
 			VersionLabel.Show();
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+		{
+			this.Invoke(new MethodInvoker(() =>
+			{
+				SearchURLBOX.Text = e.Address;
+			}
+			));
+		}
+
+		private void BookmarkList_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void VersionLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void BookmarkSet_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Settings_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void BookmarkPage_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ColorLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void HomeSet_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void HomePageLabel_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void PersonalizationPage_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void BookmarkImage_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void CloseButton_Click(object sender, EventArgs e)
+		{
+			SettingsMenu.Hide();
+			Settings.Hide();
+			PersonalizationLabel.Hide();
+			PersonalizationImage.Hide();
+			HomePageLabel.Hide();
+			HomeSet.Hide();
+			ColorLabel.Hide();
+			pictureBox11.Hide();
+			pictureBox12.Hide();
+			pictureBox13.Hide();
+			pictureBox14.Hide();
+			pictureBox15.Hide();
+			pictureBox16.Hide();
+			pictureBox17.Hide();
+			pictureBox18.Hide();
+			pictureBox19.Hide();
+			pictureBox20.Hide();
+			pictureBox21.Hide();
+			pictureBox22.Hide();
+			pictureBox23.Hide();
+			pictureBox24.Hide();
+			pictureBox25.Hide();
+			pictureBox26.Hide();
+			pictureBox27.Hide();
+			pictureBox28.Hide();
+			pictureBox29.Hide();
+			pictureBox30.Hide();
+			pictureBox31.Hide();
+			pictureBox32.Hide();
+			pictureBox33.Hide();
+			pictureBox34.Hide();
+			pictureBox35.Hide();
+			pictureBox36.Hide();
+			pictureBox37.Hide();
+			pictureBox38.Hide();
+			pictureBox39.Hide();
+			pictureBox41.Hide();
+			pictureBox42.Hide();
+			pictureBox43.Hide();
+			pictureBox44.Hide();
+			pictureBox45.Hide();
+			pictureBox46.Hide();
+			pictureBox47.Hide();
+			pictureBox48.Hide();
+			pictureBox49.Hide();
+			pictureBox50.Hide();
+			pictureBox52.Hide();
+			pictureBox53.Hide();
+			pictureBox54.Hide();
+			pictureBox55.Hide();
+			pictureBox56.Hide();
+			pictureBox57.Hide();
+			pictureBox58.Hide();
+			pictureBox59.Hide();
+			BookmarkList.Hide();
+			BookmarkSet.Hide();
+			BookmarkLabel.Hide();
+			BookmarkImage.Hide();
+			SubtractBookmarkButton.Hide();
+			SubtractBookmarkButton.Hide();
+			CloseButton.Hide();
+			VersionLabel.Hide();
+		}
+
+		private void PersonalizationLabel_Click_1(object sender, EventArgs e)
+		{
+			PersonalizationLabel.Show();
+			PersonalizationImage.Show();
+			HomePageLabel.Show();
+			HomeSet.Show();
+			ColorLabel.Show();
+			pictureBox11.Show();
+			pictureBox12.Show();
+			pictureBox13.Show();
+			pictureBox14.Show();
+			pictureBox15.Show();
+			pictureBox16.Show();
+			pictureBox17.Show();
+			pictureBox18.Show();
+			pictureBox19.Show();
+			pictureBox20.Show();
+			pictureBox21.Show();
+			pictureBox22.Show();
+			pictureBox23.Show();
+			pictureBox24.Show();
+			pictureBox25.Show();
+			pictureBox26.Show();
+			pictureBox27.Show();
+			pictureBox28.Show();
+			pictureBox29.Show();
+			pictureBox30.Show();
+			pictureBox31.Show();
+			pictureBox32.Show();
+			pictureBox33.Show();
+			pictureBox34.Show();
+			pictureBox35.Show();
+			pictureBox36.Show();
+			pictureBox37.Show();
+			pictureBox38.Show();
+			pictureBox39.Show();
+			pictureBox41.Show();
+			pictureBox42.Show();
+			pictureBox43.Show();
+			pictureBox44.Show();
+			pictureBox45.Show();
+			pictureBox46.Show();
+			pictureBox47.Show();
+			pictureBox48.Show();
+			pictureBox49.Show();
+			pictureBox50.Show();
+			pictureBox52.Show();
+			pictureBox53.Show();
+			pictureBox54.Show();
+			pictureBox55.Show();
+			pictureBox56.Show();
+			pictureBox57.Show();
+			pictureBox58.Show();
+			pictureBox59.Show();
+			BookmarkList.Hide();
+			BookmarkSet.Hide();
+			SubtractBookmarkButton.Hide();
+		}
+
+		private void BookmarkLabel_Click_1(object sender, EventArgs e)
+		{
+			HomePageLabel.Hide();
+			HomeSet.Hide();
+			ColorLabel.Hide();
+			pictureBox11.Hide();
+			pictureBox12.Hide();
+			pictureBox13.Hide();
+			pictureBox14.Hide();
+			pictureBox15.Hide();
+			pictureBox16.Hide();
+			pictureBox17.Hide();
+			pictureBox18.Hide();
+			pictureBox19.Hide();
+			pictureBox20.Hide();
+			pictureBox21.Hide();
+			pictureBox22.Hide();
+			pictureBox23.Hide();
+			pictureBox24.Hide();
+			pictureBox25.Hide();
+			pictureBox26.Hide();
+			pictureBox27.Hide();
+			pictureBox28.Hide();
+			pictureBox29.Hide();
+			pictureBox30.Hide();
+			pictureBox31.Hide();
+			pictureBox32.Hide();
+			pictureBox33.Hide();
+			pictureBox34.Hide();
+			pictureBox35.Hide();
+			pictureBox36.Hide();
+			pictureBox37.Hide();
+			pictureBox38.Hide();
+			pictureBox39.Hide();
+			pictureBox41.Hide();
+			pictureBox42.Hide();
+			pictureBox43.Hide();
+			pictureBox44.Hide();
+			pictureBox45.Hide();
+			pictureBox46.Hide();
+			pictureBox47.Hide();
+			pictureBox48.Hide();
+			pictureBox49.Hide();
+			pictureBox50.Hide();
+			pictureBox52.Hide();
+			pictureBox53.Hide();
+			pictureBox54.Hide();
+			pictureBox55.Hide();
+			pictureBox56.Hide();
+			pictureBox57.Hide();
+			pictureBox58.Hide();
+			pictureBox59.Hide();
+			BookmarkList.Hide();
+			BookmarkSet.Hide();
+			BookmarkLabel.Hide();
+			BookmarkImage.Hide();
+			BookmarkList.Show();
+			BookmarkSet.Show();
+			BookmarkLabel.Show();
+			BookmarkImage.Show();
+			SubtractBookmarkButton.Show();
+		}
+
+		private void SubtractBookmarkButton_Click_1(object sender, EventArgs e)
+		{
+			BookmarkList.Items.Clear();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
--- Web Browser/Properties/Resources.Designer.cs
+++ Web Browser/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Web_Browser.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -106,6 +106,16 @@ namespace Web_Browser.Properties {
         internal static System.Drawing.Bitmap Home {
             get {
                 object obj = ResourceManager.GetObject("Home", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NewTab {
+            get {
+                object obj = ResourceManager.GetObject("NewTab", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
